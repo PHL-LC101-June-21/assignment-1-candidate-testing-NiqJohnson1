ca69df3c3d59c2b7d89d822ca559ab5cd9c8cd46
Niquelle J <41593517+NiqJohnson1@users.noreply.github.com>
2021-06-14 17:36:24 +0000

    This is part 1 of the assignment. When the user inputs the correct answer it will let them know it is correct.

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "Paul";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "1. Who was the first American woman in space? "
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "Sally Ride";
 let questions;
 let correctAnswers;
 let candidateAnswers;
@@ -15,12 +15,16 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+const input = require('readline-sync')
+ input.question("What is your name?")
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+input.question(question)
 
+console.log("Your Answer:", candidateAnswer);
+console.log("Correct Answer:", correctAnswer);
 
 }
 
@@ -28,7 +32,15 @@ function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
+  //if the candidate's answer matches the correct answer, print "Yes, you are correct"
+  //Otherwise if the candidate's answer is incorrect, print "Incorrect answer"
 
+if (candidateAnswer && correctAnswer) {
+  console.log("Yes you are correct!");
+}
+else  {
+    console.log("Incorrect answer");
+  }
   let grade;
   
 
@@ -38,7 +50,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+      console.log("Hello", candidateName);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
